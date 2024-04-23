f59bc5baeb0980a5fbbb8a681f36abb2dbfe2bf5
Povelitelpelmeney <120256800+Povelitelpelmeney@users.noreply.github.com>
2024-04-23 13:21:06 +0500

    Update kmp.py

--- algo/kmp.py
+++ algo/kmp.py
@@ -3,35 +3,41 @@ from algo.super import Super
 
 class KMP(Super):
     @staticmethod
-    def find(substring, text):
-        sm_length = len(substring)
-        length = len(text)
-        pre = KMP.make_pre(substring, text)
-        k1 = 0
-        i1 = 0
-        while i1 < length - 1:
-            if substring[k1] == text[i1]:
-                k1 += 1
-                i1 += 1
-                if k1 == sm_length:
-                    yield i1 - sm_length
-                    k1 = pre[k1 - 1]
-            elif k1 > 0:
-                k1 = pre[k1 - 1]
+    def find(text: str, substring: str):
+        substring_len = len(substring)
+        text_len = len(text)
+        if not text_len or substring_len > text_len:
+            return []
+        pre = func_prefix(substring)
+        i = 0
+        j = 0
+        while i < text_len:
+            if text[i] == substring[j]:
+                if j == substring_len - 1:
+                    yield i - substring_len + 1
+                    j = pre[j]
+                else:
+                    j += 1
+                i += 1
+            elif j:     
+                j = pre[j-1]
             else:
-                i1 += 1
+                i += 1
 
     @staticmethod
-    def make_pre(substring, text):
-        sm_length = len(substring)
-        paragraph = substring + "#" + text
-        pre = []
-        pre.append(0)
-        k = 0
-        for i in range(1, sm_length + 1):
-            while k > 0 and paragraph[k] != paragraph[i]:
-                k = pre[k - 1]
-            if paragraph[k] == paragraph[i]:
-                k += 1
-            pre.append(k)
+    def func_prefix(string: str) -> list:
+        length = len(string)
+        pre = [0]*length
+        i = 0
+        j = 1
+        while j < length :
+            if string[i] == string[j]:
+                pre[j] = i + 1
+                i += 1
+                j += 1
+            elif i:        
+                i = pre[i - 1]
+            else:          
+                pre[j] = 0
+                j += 1
         return pre
